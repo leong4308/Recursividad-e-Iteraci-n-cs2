dd34c1fdad4c9c2bbb08042a45b7af3a17f43438
leong <leong@DESKTOP-A61DB78>
2023-10-27 20:08:10 -0400

    Agregar archivos de proyecto.

---
+++ Recursividad e Iteración cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TablasDeMultiplicarConWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, RESUL, T, I;
+            string linea;
+
+            Console.Write("Digite numeros: ");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            T = 1;
+
+            while (T <= NUM)
+            {
+                I = 10;
+
+                while (I >= 1)
+                {
+                    RESUL = T * I;
+                    Console.WriteLine("{0} x {1} = {2}", T, I, RESUL);
+                    I = I - 1;
+                }
+
+                T = T + 1;
+            }
+
+            Console.Write("Pulse una Tecla :");
+            Console.ReadLine();
+        }
+    }
+}
